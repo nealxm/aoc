3a20452779950cd23644ea6a113c19b5b947852a
nealxm <88364802+nealxm@users.noreply.github.com>
2024-09-04 12:01:35 -0500

    day three part two

--- src/day03/day03.go
+++ src/day03/day03.go
@@ -16,6 +16,7 @@ func Main() {
 	}
 
 	fmt.Printf("day three part one: %d\n", Part1(string(file)))
+	fmt.Printf("day three part two: %d\n", Part2(string(file)))
 }
 
 type number struct {
@@ -26,11 +27,12 @@ type number struct {
 }
 
 var (
-	numRe  = regexp.MustCompile(`\d{1,}`)
+	numRe  = regexp.MustCompile(`\d+`)
 	specRe = regexp.MustCompile(`[^\d.]`)
+	starRe = regexp.MustCompile(`\*`)
 )
 
-func processInput(input string) (numbers []number, source []string) {
+func processNumbers(input string) (numbers []number, source []string) {
 	source = strings.Split(input, "\n")
 	for i, line := range source {
 		for _, numIdx := range numRe.FindAllStringIndex(line, -1) {
@@ -50,7 +52,7 @@ func processInput(input string) (numbers []number, source []string) {
 }
 
 func Part1(input string) (sum int) {
-	numbers, source := processInput(input)
+	numbers, source := processNumbers(input)
 
 	for _, num := range numbers {
 	numLoop:
@@ -68,6 +70,62 @@ func Part1(input string) (sum int) {
 		if num.valid {
 			sum += num.value
 		}
+	}
+	return sum
+}
+
+// using cantor paring function to store 2d coords as 1d num
+// after encoding, don't need access to original 2d coords so decode is unnecessary
+func encode(a, b int) int {
+	return ((a + b) * (a + b + 1) / 2) + b
+}
+
+func processStars(input string) (map[int][]*number, []*number, []string) {
+	stars := map[int][]*number{}
+	numbers := []*number{}
+	source := strings.Split(input, "\n")
+
+	for i, line := range source {
+		for _, numIdx := range numRe.FindAllStringIndex(line, -1) {
+			val, err := strconv.Atoi(line[numIdx[0]:numIdx[1]])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			numbers = append(numbers, &number{
+				value: val,
+				row:   i,
+				col:   numIdx,
+			})
+		}
+		for _, starIdx := range starRe.FindAllStringIndex(line, -1) {
+			stars[encode(i, starIdx[0])] = []*number{}
+		}
+	}
+	return stars, numbers, source
+}
+
+func Part2(input string) (sum int) {
+	stars, numbers, source := processStars(input)
+
+	for _, num := range numbers {
+		for i := max(0, num.row-1); i <= min(len(source)-1, num.row+1); i++ {
+			for j := max(0, num.col[0]-1); j < min(len(source[i]), num.col[1]+1); j++ {
+				if i == num.row && (j >= num.col[0] && j < num.col[1]) {
+					continue
+				}
+				if starRe.Match([]byte{source[i][j]}) {
+					stars[encode(i, j)] = append(stars[encode(i, j)], num)
+				}
+			}
+		}
+	}
+
+	for _, nums := range stars {
+		if len(nums) != 2 {
+			continue
+		}
+		sum += nums[0].value * nums[1].value
 	}
 	return sum
 }
--- test/day03_test.go
+++ test/day03_test.go
@@ -22,3 +22,21 @@ func TestD3p1(t *testing.T) {
 		t.Error("day 3 part 1 failed")
 	}
 }
+
+func TestD3p2(t *testing.T) {
+	input2 := `467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`
+	expected2 := 467835
+
+	if result := day03.Part2(input2); result != expected2 {
+		t.Error("day 3 part 2 failed")
+	}
+}
