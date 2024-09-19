31624cda69b392afe0995c3b579cf6ebc9a111da
nealxm <88364802+nealxm@users.noreply.github.com>
2024-09-19 06:22:09 -0500

    better main

--- main.go
+++ main.go
@@ -14,31 +14,26 @@ import (
 	"os"
 )
 
+var funcs = map[string]func(){
+	"1": day01.Main,
+	"2": day02.Main,
+	"3": day03.Main,
+	"4": day04.Main,
+	"5": day05.Main,
+	"6": day06.Main,
+	"7": day07.Main,
+	"8": day08.Main,
+	"9": day09.Main,
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatalln("invalid arguements: must specify an aoc day to run (1-25)")
 	}
 
-	switch os.Args[1] {
-	case "1":
-		day01.Main()
-	case "2":
-		day02.Main()
-	case "3":
-		day03.Main()
-	case "4":
-		day04.Main()
-	case "5":
-		day05.Main()
-	case "6":
-		day06.Main()
-	case "7":
-		day07.Main()
-	case "8":
-		day08.Main()
-	case "9":
-		day09.Main()
-	default:
+	if day, exists := funcs[os.Args[1]]; !exists {
 		log.Fatalln("invalid day specified: either not implemented or not 1-25")
+	} else {
+		day()
 	}
 }
