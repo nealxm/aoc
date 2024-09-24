ed114399152a876ff181fe5f703961c2e2e89228
nealxm <88364802+nealxm@users.noreply.github.com>
2024-09-24 16:01:49 -0700

    day ten part two

--- src/day10/day10.go
+++ src/day10/day10.go
@@ -13,14 +13,15 @@ func Main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("day nine part one: %d\n", part1(string(file)))
+	fmt.Printf("day ten part one: %d\n", part1(string(file)))
+	fmt.Printf("day ten part two: %d\n", part2(string(file)))
 }
 
 type vec2 struct {
 	r, c int
 }
 
-func processInput(input string) (grid [][]string, start vec2) {
+func processInput1(input string) (grid [][]string, start vec2) {
 	for r, line := range strings.Split(input, "\n") {
 		row := []string{}
 
@@ -43,20 +44,11 @@ var checks = [4]vec2{
 	{0, -1},
 }
 
-func clamp(val, min, max int) int {
-	if val < min {
-		return min
-	} else if val > max {
-		return max
-	}
-	return val
-}
-
 var valids = [4]string{
-	"|LJS",
-	"-LFS",
-	"|7FS",
-	"-J7S",
+	"|LJ",
+	"-LF",
+	"|7F",
+	"-J7",
 }
 
 var dirs = map[string]func(idx int) bool{
@@ -69,19 +61,22 @@ var dirs = map[string]func(idx int) bool{
 }
 
 func part1(input string) (steps int) {
-	grid, curr := processInput(input)
+	grid, curr := processInput1(input)
 	prev := curr
 
 outer:
 	for {
 	main:
 		for i, check := range checks {
-			r := clamp(curr.r+check.r, 0, len(grid)-1)
-			c := clamp(curr.c+check.c, 0, len(grid[r])-1)
+			r := min(max(curr.r+check.r, 0), len(grid)-1)
+			c := min(max(curr.c+check.c, 0), len(grid[r])-1)
 			if r == prev.r && c == prev.c {
 				continue
 			}
 			currChr := grid[curr.r][curr.c]
+			if currChr == "S" {
+				currChr = "|-LJ7F"
+			}
 
 			switch grid[r][c] {
 			case "|", "-", "L", "J", "7", "F":
@@ -100,4 +95,145 @@ outer:
 		}
 	}
 	return steps / 2
+}
+
+func encode(a, b int) int {
+	return ((a + b) * (a + b + 1) / 2) + b
+}
+
+func processInput2(input string) ([][]string, map[int]bool) {
+	grid := [][]string{}
+	curr := vec2{}
+
+	for r, line := range strings.Split(input, "\n") {
+		row := []string{}
+
+		for c, char := range line {
+			row = append(row, string(char))
+
+			if char == 'S' {
+				curr = vec2{r, c}
+			}
+		}
+		grid = append(grid, row)
+	}
+	prev := curr
+	loop := make(map[int]bool)
+
+outer:
+	for {
+	main:
+		for i, check := range checks {
+			r := min(max(curr.r+check.r, 0), len(grid)-1)
+			c := min(max(curr.c+check.c, 0), len(grid[r])-1)
+			if r == prev.r && c == prev.c {
+				continue
+			}
+			currChr := grid[curr.r][curr.c]
+			if currChr == "S" {
+				currChr = "|-LJ7F"
+			}
+
+			switch grid[r][c] {
+			case "|", "-", "L", "J", "7", "F":
+				if dirs[grid[r][c]](i) && strings.ContainsAny(currChr, valids[i]) {
+					prev = curr
+					curr = vec2{r, c}
+					loop[encode(r, c)] = true
+					break main
+				}
+			case "S":
+				if strings.ContainsAny(currChr, valids[i]) {
+					curr = vec2{r, c}
+					loop[encode(r, c)] = true
+					break outer
+				}
+			}
+		}
+	}
+	vAdj, j := [2]int{}, 0
+
+	for i, check := range checks {
+		sAdj := vec2{
+			r: min(max(curr.r+check.r, 0), len(grid)-1),
+			c: min(max(curr.c+check.c, 0), len(grid[curr.r])-1),
+		}
+
+		if chrAdj := grid[sAdj.r][sAdj.c]; chrAdj != "." && chrAdj != "S" && dirs[chrAdj](i) {
+			vAdj[j] = i
+			j++
+		}
+	}
+
+	chrs := make(map[rune]bool)
+	for _, chr := range valids[vAdj[0]] {
+		chrs[chr] = true
+	}
+	for _, chr := range valids[vAdj[1]] {
+		if chrs[chr] {
+			grid[curr.r][curr.c] = string(chr)
+			break
+		}
+	}
+	return grid, loop
+}
+
+type strStack []string
+
+func (s *strStack) push(str string) {
+	*s = append(*s, str)
+}
+
+func (s *strStack) peek() string {
+	return (*s)[len(*s)-1]
+}
+
+func (s *strStack) pop() string {
+	str := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return str
+}
+
+func part2(input string) (area int) {
+	grid, loop := processInput2(input)
+
+	for r, row := range grid {
+		for c := range row {
+			if loop[encode(r, c)] {
+				continue
+			}
+			stack := strStack{}
+
+			for i, chr := range row[c:] {
+				if loop[encode(r, c+i)] {
+					switch chr {
+					case "|", "L", "F":
+						stack.push(chr)
+					case "-":
+						continue
+					case "J":
+						switch stack.peek() {
+						case "L":
+							stack.pop()
+						case "F":
+							stack.pop()
+							stack.push("|")
+						}
+					case "7":
+						switch stack.peek() {
+						case "F":
+							stack.pop()
+						case "L":
+							stack.pop()
+							stack.push("|")
+						}
+					}
+				}
+			}
+			if len(stack)%2 == 1 {
+				area++
+			}
+		}
+	}
+	return area
 }
--- src/day10/day10_test.go
+++ src/day10/day10_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-var example1a, example1b, example2a, example2b, input *string
+var example1a, example1b, example2a, example2b, example3a, example3b, example4a, example4b, input *string
 
 func TestMain(m *testing.M) {
 	example1aFile, err := os.ReadFile("./data/example1a.txt")
@@ -25,13 +25,29 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	example3aFile, err := os.ReadFile("./data/example3a.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	example3bFile, err := os.ReadFile("./data/example3b.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	example4aFile, err := os.ReadFile("./data/example4a.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	example4bFile, err := os.ReadFile("./data/example4b.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 	inputFile, err := os.ReadFile("./data/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	e1a, e1b, e2a, e2b, i := string(example1aFile), string(example1bFile), string(example2aFile), string(example2bFile), string(inputFile)
-	example1a, example1b, example2a, example2b, input = &e1a, &e1b, &e2a, &e2b, &i
+	e1a, e1b, e2a, e2b, e3a, e3b, e4a, e4b, i := string(example1aFile), string(example1bFile), string(example2aFile), string(example2bFile), string(example3aFile), string(example3bFile), string(example4aFile), string(example4bFile), string(inputFile)
+	example1a, example1b, example2a, example2b, example3a, example3b, example4a, example4b, input = &e1a, &e1b, &e2a, &e2b, &e3a, &e3b, &e4a, &e4b, &i
 
 	code := m.Run()
 	os.Exit(code)
@@ -56,6 +72,24 @@ func TestD10p1(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			if result := part1(*tc.input); result != tc.expected {
 				t.Errorf("(d10p1) got %d, need %d", result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestD10p2(t *testing.T) {
+	testCases := []testCase{
+		{"example3a", example3a, 4},
+		{"example3b", example3b, 4},
+		{"example4a", example4a, 8},
+		{"example4b", example4b, 10},
+		{"input", input, 363},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if result := part2(*tc.input); result != tc.expected {
+				t.Errorf("(d10p2) got %d, need %d", result, tc.expected)
 			}
 		})
 	}
