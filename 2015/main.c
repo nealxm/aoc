48637e4f9c4b85680b01c7ec79d14b6f352b3b3b
nealxm <88364802+nealxm@users.noreply.github.com>
2025-06-30 21:59:23 -0500

    2015: init

---
+++ 2015/main.c
@@ -0,0 +1,27 @@
+#include "day01.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    const char* day;
+    void (*func)(void);
+} day;
+
+static day days[] = {
+    {"1", day01_main}
+};
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "invalid arguements: must specify an aoc day to run (1-25)\n");
+        return 1;
+    }
+    int d = atoi(argv[1]);
+
+    if (d < 1 || d > 25) {
+        fprintf(stderr, "invalid day: either not implemented or not 1-25\n");
+    } else {
+        days[d-1].func();
+    }
+    return 0;
+}
